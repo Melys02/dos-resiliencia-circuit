94b900d413ada8eb752b19db3cd4d20d94496048
Melys02 <melys0919@gmail.com>
2024-10-21 20:51:15 -0500

    Controller terminado

--- src/main/java/com/example/demo/controller/MaterialController.java
+++ src/main/java/com/example/demo/controller/MaterialController.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.MaterialService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialController {
 
 
+    private final MaterialService materialService;
+
+    @GetMapping
+    @CircuitBreaker(name = "myservice", fallbackMethod = "fallbackDisponibility")
+    public String checkDisponibilidad(Long id) {
+        return materialService.checkDisponibility(id);
+    }
+
+    public String fallbackDisponibility(Long id, Throwable throwable) {
+        // Retorna una respuesta predeterminada en caso de fallo
+        return "No disponible temporalmente. Por favor intente más tarde.";
+    }
 }
